0989d259d2f84d4c8eeafe723fa6a67316a8d435
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-08-04 14:00:59 +0300

    ???

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -67,9 +67,49 @@ new Swiper('.products__slider', {
   breakpoints: {
     320: {
       slidesPerView: 1,
+      spaceBetween: 0,
     },
     768: {
-      slidesPerView: 2.6,
+      slidesPerView: 4,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+});
+
+new Swiper('.buy-now__slider', {
+  pagination: {
+    el: '.swiper__pagination',
+    clickable: true,
+  },
+  grabCursor: true,
+  // slideToClickedSlide: true,
+  keyboard: {
+    enabled: true,
+    onlyinViewport: true,
+  },
+  // mousewheel: {
+  //   sensitivity: 2,
+  //   eventsTarget: '.reviews__slider',
+  // },
+  freeMode: true,
+  simulateTouch: false,
+  autoplay: {
+    delay: 2000,
+    stopOnLastSlide: false,
+    disableonInteraction: true,
+  },
+  speed: 750,
+  // effect: 'coverflow'
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
       spaceBetween: 18,
     },
     1200: {
