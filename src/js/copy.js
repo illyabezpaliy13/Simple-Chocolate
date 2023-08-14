cef98a74c13ef0930b2d4c2c7a931c2b51e6856a
m1xture <109725502+m1xture@users.noreply.github.com>
2023-08-14 22:15:57 +0300

    yeeeees. i gonna have a good night

--- src/js/copy.js
+++ src/js/copy.js
@@ -1,7 +1,19 @@
-function copy() {
-  let copyText = 'Simply Chocolate';
-  copyText.select();
-  document.execCommand('copy');
-}
+// console.log("laala");
 
-document.querySelector('#copy').addEventListener('click', copy);
+// function copy() {
+//   // document.querySelector('#copy').classList.toggle('submenu-selected__icon');
+//   let copyText = 'Simply Chocolate';
+//   copyText.select();
+//   document.execCommand('copy');
+// }
+
+// document.querySelector('#copy').addEventListener('click', copy);
+
+
+// console.log(document.getElementById('#copyButton'));
+
+// document.getElementById('copyButton').addEventListener('click', () => {
+//   document
+//     .getElementById('copyButton')
+//     .classList.toggle('submenu-selected__icon');
+// });
--- src/js/share.js
+++ src/js/share.js
@@ -1,3 +1,8 @@
+const copyBtn0 = document.getElementById('copyButton');
+const copyBtn1 = document.getElementById('copyButton1');
+const copyBtn2 = document.getElementById('copyButton2');
+const copyBtn3 = document.getElementById('copyButton3');
+
 const textToCopy = document.getElementById('textToCopy');
 document.addEventListener('DOMContentLoaded', function () {
   const copyButton = document.getElementById('copyButton');
@@ -5,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   copyButton.addEventListener('click', function () {
     const text = textToCopy.textContent;
-
+    copyBtn0.firstElementChild.classList.toggle('submenu-selected__icon');
     const tempInput = document.createElement('input');
     tempInput.value = text;
     document.body.appendChild(tempInput);
@@ -21,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const copyButton1 = document.getElementById('copyButton1');
   copyButton1.addEventListener('click', function () {
     const text = textToCopy.textContent;
-
+    copyBtn1.firstElementChild.classList.toggle('submenu-selected__icon');
     const tempInput = document.createElement('input');
     tempInput.value = text;
     document.body.appendChild(tempInput);
@@ -37,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const copyButton2 = document.getElementById('copyButton2');
   copyButton2.addEventListener('click', function () {
     const text = textToCopy.textContent;
-
+    copyBtn2.firstElementChild.classList.toggle('submenu-selected__icon');
     const tempInput = document.createElement('input');
     tempInput.value = text;
     document.body.appendChild(tempInput);
@@ -53,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const copyButton3 = document.getElementById('copyButton3');
   copyButton3.addEventListener('click', function () {
     const text = textToCopy.textContent;
-
+    copyBtn3.firstElementChild.classList.toggle('submenu-selected__icon');
     const tempInput = document.createElement('input');
     tempInput.value = text;
     document.body.appendChild(tempInput);
